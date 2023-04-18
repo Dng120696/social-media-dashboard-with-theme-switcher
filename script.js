92fef51c4105dc53b17fa9a0d7cdea658d74f56e
Dng16 <dongyang0016@gmail.com>
2023-04-18 13:17:40 +0800

    initial commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const body = document.querySelector('body');
 const toggle = document.querySelector('.toggle-box');
 const darkMode = document.querySelector('.dark-mode');
 const header = document.querySelector('header');
-
+const toggleName = document.querySelector('.toggle-icon-name');
 
 toggle.addEventListener('click',()=>{
   darkMode.classList.toggle('light-mode');
@@ -10,5 +10,11 @@ toggle.addEventListener('click',()=>{
   body.classList.toggle('active');
   header.classList.toggle('light-mode')
 
+  if(toggle.classList.contains('light-mode')){
+      toggleName.textContent = 'Light Mode';
+    }else{
+      toggleName.textContent = 'Dark Mode';
+    }
+    
 });
 
