4bdfd726a360ace5d26a7bd85929aa40ea9096f8
Dng16 <dongyang0016@gmail.com>
2023-07-02 11:20:21 +0800

    final commit

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
-const body = document.querySelector('body');
-const toggle = document.querySelector('.toggle-box');
-const darkMode = document.querySelector('.dark-mode');
-const header = document.querySelector('header');
-const toggleName = document.querySelector('.toggle-icon-name');
+const body = document.querySelector("body");
+const toggle = document.querySelector(".toggle-box");
+const darkModeButton = document.querySelector(".dark-mode");
+const header = document.querySelector("header");
+const toggleName = document.querySelector(".toggle-icon-name");
 
-toggle.addEventListener('click',()=>{
-  darkMode.classList.toggle('light-mode');
-  toggle.classList.toggle('light-mode');
-  body.classList.toggle('active');
-  header.classList.toggle('light-mode')
+const isDarkMode = JSON.parse(localStorage.getItem("isDarkMode"));
 
-  if(toggle.classList.contains('light-mode')){
-      toggleName.textContent = 'Light Mode';
-    }else{
-      toggleName.textContent = 'Dark Mode';
-    }
-    
+function setTheme(darkMode) {
+  if (darkMode) {
+    toggleName.textContent = "Light Mode";
+    body.classList.add("active");
+    header.classList.add("light-mode");
+    darkModeButton.classList.add("light-mode");
+  } else {
+    toggleName.textContent = "Dark Mode";
+    body.classList.remove("active");
+    header.classList.remove("light-mode");
+    darkModeButton.classList.remove("light-mode");
+  }
+}
+
+// Check local storage for user preference and apply the appropriate theme
+if (isDarkMode !== null) {
+  setTheme(isDarkMode);
+} else {
+  // If no preference is found in local storage, set the default theme
+  setTheme(false);
+}
+
+toggle.addEventListener("click", function () {
+  darkModeButton.classList.toggle("light-mode");
+  this.classList.toggle("light-mode");
+  const currentMode = body.classList.contains("active");
+  localStorage.setItem("isDarkMode", JSON.stringify(!currentMode));
+  setTheme(!currentMode);
 });
 
